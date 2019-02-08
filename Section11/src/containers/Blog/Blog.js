1f1cd571edeaa8e2fc54bef7a9d30ec5e03f1442
PatrickPiquette <ppiquette@elementai.com>
2019-02-08 15:20:41 -0500

    239 done

--- Section11/src/containers/Blog/Blog.js
+++ Section11/src/containers/Blog/Blog.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts'
 import { Route , NavLink, Switch, Redirect } from 'react-router-dom'
-import NewPost from './NewPost/NewPost'
-
-
 import './Blog.css';
+
+import asyncComponent from '../../hoc/asyncComponent'
+// import NewPost from './NewPost/NewPost'
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost')
+})
+
 
 class Blog extends Component {
     state = {
-        authenticated: false
+        authenticated: true
     }
     
     render () {
@@ -32,6 +37,7 @@ class Blog extends Component {
                                     hash: "submit",
                                     search: "quick-submit=true"
                                 }}>New Post</NavLink></li>
+                            <li style={{color: "lightgray", fontSize: 10 }}>React Version: {React.version}</li>
                         </ul>
                     </nav>
                 </header>
@@ -39,7 +45,7 @@ class Blog extends Component {
                 {/* Switch component is used to tell Router to stop when it finds one valid route. Otherwise,
                     it would display *all* matching routes */}
                 <Switch>
-                    {this.state.authenticated ? <Route path="/new-post" exact component={NewPost}/> : null}
+                    {this.state.authenticated ? <Route path="/new-post" exact component={AsyncNewPost}/> : null}
                     <Route path="/posts" component={Posts}/>
                     <Redirect from="/" to="/posts"/>
                 </Switch>
