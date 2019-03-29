1460c9187a8b9fc519eac4b49f35a337f3c669aa
PatrickPiquette <ppiquette@elementai.com>
2019-03-29 16:57:53 -0400

    280 done

---
+++ Section14/src/SimpleRedux-inNode.js
@@ -0,0 +1,45 @@
+// 
+// 
+// 
+// This file is not attached to the rest of this folder project. Do the following to run:
+// node src/SimpleRedux-inNode.js
+// 
+// 
+// 
+
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0,
+    patate: 8,
+}
+
+// Reducer
+const rootReducer = (state = initialState, action) => {
+    let newValue = 0
+    switch(action.type){
+        case "INC_COUNTER":
+            newValue = state.counter + 1;
+            return { ...state, counter: newValue}
+        
+        case "ADD_COUNTER":
+            newValue = state.counter + action.value;
+            return { ...state, counter: newValue}
+        
+        default:
+            return state;  
+    }
+}
+
+// Create store
+console.log("Create Store");
+const store = createStore(rootReducer);
+console.log(store);
+console.log(store.getState());
+
+// Dispatching actions
+console.log("Dispatch some actions");
+store.dispatch({type: "INC_COUNTER"});
+store.dispatch({type: "ADD_COUNTER", value: 10});
+console.log(store.getState());
